97150f1883b57ca263fcb19cc6ee80adf35af413
vadimkirov <vadimkirov@yandex.ru>
2020-03-05 12:00:02 +0300

    my HW2 without optional

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -11,7 +11,23 @@ public class Meal {
 
     private final int calories;
 
+//    private Integer userId;
+
+//    public Meal(Integer id, LocalDateTime dateTime, String description, int calories, Integer userId) {
+//        super(id);
+//        this.dateTime = dateTime;
+//        this.description = description;
+//        this.calories = calories;
+//        this.userId = userId;
+//    }
+
     public Meal(LocalDateTime dateTime, String description, int calories) {
+        this(null, dateTime, description, calories);
+    }
+
+    public Meal(Integer id, LocalDateTime dateTime, String description, int calories) {
+        super(id);
+//        this.id = id;
         this.dateTime = dateTime;
         this.description = description;
         this.calories = calories;
